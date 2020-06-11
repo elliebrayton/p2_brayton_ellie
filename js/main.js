ea47161ad4a9ace6daa6f57d0d1027b6f66c66ad
Ellie Brayton <ebrayton@ufl.edu>
2020-06-11 11:29:18 -0500

    Worked on header and hero img

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/* Burger nav */
+$('.burger, .overlay').click(function(){
+$('.burger').toggleClass('clicked');
+$('.overlay').toggleClass('show');
+$('.mobile-menu').toggleClass('show');
+$('body').toggleClass('overflow');
+});
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
